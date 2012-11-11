0a3b312a9b75b4f6a9e59f4c2e176d7a62338a12
unknown <smith510066@yahoo.com>
2012-11-11 16:14:52 -0600

    added dynamic title function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+	def full_title(page_title)
+		base_title = "ruby on rails demo";
+		if page_title.empty?
+			page_title = base_title
+		else
+			"#{base_title} #{page_title}"
+		end
+	end
+
+	def test_function
+		
+	end
 end
